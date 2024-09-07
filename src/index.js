1332c287a96eab116fd42df53e52fc61f3162927
EpicZuko <zukokalilov5@gmail.com>
2024-09-07 16:08:57 +0600

    created

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./services/index";
 import { Provider } from "react-redux";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
